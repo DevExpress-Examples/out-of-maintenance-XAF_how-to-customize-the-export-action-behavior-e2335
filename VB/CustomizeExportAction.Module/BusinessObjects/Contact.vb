ebe914ed5ea6c9b3c66f4262cefb15a28e8e9467
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomizeExportAction.Module/BusinessObjects/Contact.vb
@@ -0,0 +1,62 @@
+﻿Imports System
+Imports System.ComponentModel
+
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Validation
+
+Namespace CustomizeExportAction.Module.BusinessObjects
+   <DefaultClassOptions> _
+   Public Class Contact
+       Inherits BaseObject
+
+      Public Sub New(ByVal session As Session)
+          MyBase.New(session)
+         ' This constructor is used when an object is loaded from a persistent storage.
+         ' Do not place any code here or place it only when the IsLoading property is false:
+         ' if (!IsLoading){
+         '    It is now OK to place your initialization code here.
+         ' }
+         ' or as an alternative, move your initialization code into the AfterConstruction method.
+      End Sub
+      Public Overrides Sub AfterConstruction()
+         MyBase.AfterConstruction()
+         ' Place here your initialization code.
+      End Sub
+
+      Private lastName_Renamed As String
+      Public Property LastName() As String
+         Get
+            Return lastName_Renamed
+         End Get
+         Set(ByVal value As String)
+            SetPropertyValue("LastName", lastName_Renamed, value)
+         End Set
+      End Property
+
+      Private firstName_Renamed As String
+      Public Property FirstName() As String
+         Get
+            Return firstName_Renamed
+         End Get
+         Set(ByVal value As String)
+            SetPropertyValue("FirstName", firstName_Renamed, value)
+         End Set
+      End Property
+
+      Private department_Renamed As String
+      Public Property Department() As String
+         Get
+            Return department_Renamed
+         End Get
+         Set(ByVal value As String)
+            SetPropertyValue("Department", department_Renamed, value)
+         End Set
+      End Property
+   End Class
+
+End Namespace
---
+++ VB/CustomizeExportAction.Module/Controllers/CustomizeExportController.vb
@@ -0,0 +1,59 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Text
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.Persistent.Base
+
+Imports DevExpress.ExpressApp.SystemModule
+
+Imports DevExpress.XtraPrinting
+
+Namespace CustomizeExportAction.Module.Controllers
+   Partial Public Class CustomizeExportController
+       Inherits ViewController
+
+      Public Sub New()
+         InitializeComponent()
+         RegisterActions(components)
+         TargetViewType = ViewType.ListView
+      End Sub
+      Private exportController As ExportController
+      Protected Overrides Sub OnActivated()
+         MyBase.OnActivated()
+         exportController = Frame.GetController(Of ExportController)()
+         AddHandler exportController.CustomGetDefaultFileName, AddressOf exportController_CustomGetDefaultFileName
+         AddHandler exportController.CustomExport, AddressOf CustomExport
+      End Sub
+
+      Protected Overridable Sub CustomExport(ByVal sender As Object, ByVal e As CustomExportEventArgs)
+         'Customize Export Options
+         If e.ExportTarget = ExportTarget.Xls Then
+            Dim options As XlsExportOptions = CType(e.ExportOptions, XlsExportOptions)
+            If options Is Nothing Then
+               options = New XlsExportOptions()
+            End If
+            options.SheetName = View.Caption
+            options.ShowGridLines = True
+            e.ExportOptions = options
+         End If
+      End Sub
+      Private Sub exportController_CustomGetDefaultFileName(ByVal sender As Object, ByVal e As CustomGetDefaultFileNameEventArgs)
+#If EASYTEST Then
+             'Provide a custom file name
+         e.FileName = e.FileName & "_06.25.12"
+#Else
+          'Provide a custom file name
+         e.FileName = e.FileName & "_" & Date.Now.ToString("MM.dd.yy")
+#End If
+      End Sub
+      Protected Overrides Sub OnDeactivated()
+         RemoveHandler exportController.CustomGetDefaultFileName, AddressOf exportController_CustomGetDefaultFileName
+         RemoveHandler exportController.CustomExport, AddressOf CustomExport
+         MyBase.OnDeactivated()
+      End Sub
+   End Class
+End Namespace
